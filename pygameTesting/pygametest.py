b7fcf3100e54cee21323db06f689222ec055a851
jackh367 <43012239+jackh367@users.noreply.github.com>
2019-01-18 14:59:35 -0500

    push

--- pygameTesting/pygametest.py
+++ pygameTesting/pygametest.py
@@ -1,49 +1,60 @@
+# Import and initialize pygame module.
 import pygame
 pygame.init()
 
+# Set up game window.
+screen = pygame.display.set_mode((1600,700))
+pygame.display.set_caption("Ram Ranch: The Video Game alpha")
 
-win = pygame.display.set_mode((500, 500))
-
-pygame.display.set_caption("Ram Ranch: The Video Game ULTRA PRE ALPHA")
-
-x = 50
-y = 425
-width = 40
-height = 60
-vel = 5
-
-
-screen = pygame.display.set_mode((1600,800))
-pygame.init()
-
+# Setting up sounds.
 file = 'Ram Ranch.mp3'
-pygame.init()
 pygame.mixer.init()
 pygame.mixer.music.load(file)
 pygame.mixer.music.play()
 pygame.event.wait()
 
+# Setting up ram.
+ramimg = pygame.image.load("ram.png")
+ramx = 50
+ramy = 425
+ramwidth = 40
+ramheight = 60
+ramvel = 5
+
+# Setting up the ranch.
+ranchimg = pygame.image.load("ranch.jpg")
+ranchx = 0
+ranchy = 0
+ranchwidth = 1920
+ranchheight = 734
+
+# Start game loop.
 run = True
 while run:
-    pygame.time.delay(10)
 
+    # pygame.time.delay(10)
+
+    # Process events.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
+    # Get user input and move the ram.
     keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and ramx > ramvel:
+        ramx -= ramvel
+    if keys[pygame.K_RIGHT] and ramx < 1600 - ramwidth - ramvel:
+        ramx += ramvel
+    if keys[pygame.K_UP] and ramy > ramvel:
+        ramy -= ramvel
+    if keys[pygame.K_DOWN] and ramy < 800 - ramheight - ramvel:
+        ramy += ramvel
 
-    if keys[pygame.K_LEFT] and x > vel:
-        x -= vel
-    if keys[pygame.K_RIGHT] and x < 1600 - width - vel:
-        x += vel
-    if keys[pygame.K_UP] and y > vel:
-        y -= vel
-    if keys[pygame.K_DOWN] and y < 800 - height - vel:
-        y += vel
+    # Render the scene.
+    screen.fill((0, 0, 0))
+    screen.blit(ranchimg, (ranchx, ranchy))
+    screen.blit(ramimg, (ramx, ramy))
 
-    win.fill((0, 0, 0))
-    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
     pygame.display.update()
 
 pygame.quit()
