66eeb792cc07d309ccd9e53eabead2aa5f0f8773
jackh367 <43012239+jackh367@users.noreply.github.com>
2019-01-21 14:56:52 -0500

    pushed

--- pygameTesting/pygametest.py
+++ pygameTesting/pygametest.py
@@ -1,6 +1,6 @@
 # Import and initialize pygame module.
 import pygame
-import webbrowser
+from random import choice
 pygame.init()
 
 # Set up game window.
@@ -11,8 +11,17 @@ pygame.display.set_caption("Ram Ranch: The Video Game")
 file = 'Ram Ranch.mp3'
 pygame.mixer.init()
 pygame.mixer.music.load(file)
-#pygame.mixer.music.play()
+pygame.mixer.music.play()
 pygame.event.wait()
+#munch sound effect when ram eats grass
+def munch():
+    file = 'munch.mp3'
+    pygame.mixer.init()
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play()
+    pygame.event.wait()
+
+
 
 # Setting up ram.
 ramleft = pygame.image.load("ram.png")
@@ -22,7 +31,22 @@ ramx = 50
 ramy = 425
 ramwidth = 40
 ramheight = 60
-ramvel = 5
+ramvel = 20
+
+# this variable keeps track of the ram's grass eaten
+score = 0
+
+def hardmode():
+    global cowboyvel
+    global ramvel
+    file = 'hardmodemusic'
+    pygame.mixer.init()
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play()
+    pygame.event.wait()
+    cowboyvel = 45
+    ramvel = 60
+
 
 # This method detects if two rectangles are overlapping.
 # Implements the algorithm found at https://www.geeksforgeeks.org/find-two-rectangles-overlap/
@@ -34,7 +58,7 @@ ramvel = 5
 # OUTPUTS:
 # returns true if the rectangles overlap and false if they don't.
 # ---------------------------------------------------------------------------------
-def collision(rect1, rect2):
+def collision(rect1, rect2, rect3):
     # Get the upper left coordinate of the first rectangle.
     l1 = (rect1[0], rect1[1])
 
@@ -47,12 +71,18 @@ def collision(rect1, rect2):
     # Get the lower right coordinate of the second triangle.
     r2 = (rect2[0] + rect2[2], rect2[1] + rect2[3])
 
+    # Get the upper left coordinate of the third triangle.
+    l3 = (rect3[0], rect3[1])
+
+    # Get the lower right coordinate of the third triangle.
+    r3 = (rect3[0] + rect3[2], rect3[1] + rect3[3])
+
     # If the rectangles do not overlap in the x-dimension, then return false.
-    if l1[0] > r2[0] or l2[0] > r1[0]:
+    if l1[0] > r2[0] or l2[0] > r1[0] or l3[0] > r3[0]:
         return False
 
     # If the rectangles do not overlap in the y-dimension, then return false.
-    if l1[1] > r2[1] or l2[1] > r1[1]:
+    if l1[1] > r2[1] or l2[1] > r1[1] or l3[1] > r3[1]:
         return False
 
     # If neither of the above if-statements are true, then the rectangles must
@@ -66,6 +96,14 @@ ranchy = 0
 ranchwidth = 1920
 ranchheight = 734
 
+# grass x,y and img
+grassx = 0
+grassy = 0
+grassimg = pygame.image.load("grass.png")
+grasswidth = 191
+grassheight = 256
+
+
 # setting up enemies
 cowboyleft = pygame.image.load("cowboyleft.png")
 cowboyright = pygame.image.load("cowboyright.png")
@@ -74,13 +112,16 @@ cowboyx = 0
 cowboyy = 0
 cowboywidth = 180
 cowboyheight = 194
-cowboyvel = 1
+cowboyvel = 3.5
 
-# setting up grass that ram runs around and eats( not started )
-
-
-
-
+# setting up grass that ram runs around and eats
+def spawngrass():
+    global grassx, grassy
+    grassx = choice(range(1000))
+    grassy = choice(range(700))
+    if collision(((grassx, grassy, grasswidth, grassheight)), ((grassx, grassy, 230, 200))):
+        grassx = choice(range(1000))
+        grassy = choice(range(700))
 
 
 # Start game loop.
@@ -122,11 +163,26 @@ while run:
     if ramy < cowboyy:
             cowboyy = cowboyy - cowboyvel
 
+    grassrect = (grassx, grassy, grasswidth, grassheight)
     cowboyrect = (cowboyx, cowboyy, cowboywidth, cowboyheight)
     ramrect = (ramx, ramy, ramwidth, ramheight)
     if collision(cowboyrect, ramrect):
-        #webbrowser.open('')
+        #webbrowser.open('https://fbcdn-sphotos-e-a.hubatka.cz/hphotos-ak-prn1/44530_491807354174208_665546187_n.jpg')
         pygame.quit()
+    if collision(ramrect, grassrect):
+        score += 1
+        munch()
+
+
+
+    if score == 50:
+        hardmode()
+
+
+
+
+
+
 
 
 
@@ -135,6 +191,7 @@ while run:
     screen.blit(ranchimg, (ranchx, ranchy))
     screen.blit(ramimg, (ramx, ramy))
     screen.blit(cowboyimg, (cowboyx, cowboyy))
+    screen.blit(grassimg, (grassx, grassy))
     pygame.display.update()
 
 
